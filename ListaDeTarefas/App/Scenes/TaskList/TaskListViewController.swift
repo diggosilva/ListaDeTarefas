d4e838173e339b55831ae27764a460a43ab6e681
Diggo Silva <rdiggosilva@gmail.com>
2026-05-13 08:34:53 -0300

    Initial configuration

---
+++ ListaDeTarefas/App/Scenes/TaskList/TaskListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TaskListViewController.swift
+//  ListaDeTarefas
+//
+//  Created by Diggo Silva on 12/05/26.
+//
+
+import UIKit
+
+class TaskListViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+   
+}
