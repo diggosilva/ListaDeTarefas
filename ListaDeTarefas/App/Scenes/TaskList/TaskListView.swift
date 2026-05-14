3b2bd613071f15a26c36176d66fb6b4b5ede6d60
Diggo Silva <rdiggosilva@gmail.com>
2026-05-14 08:33:19 -0300

    feat: create TaskListView with UITableView setup

--- ListaDeTarefas/App/Scenes/TaskList/TaskListView.swift
+++ ListaDeTarefas/App/Scenes/TaskList/TaskListView.swift
@@ -5,4 +5,44 @@
 //  Created by Diggo Silva on 13/05/26.
 //
 
-import Foundation
+import UIKit
+
+final class TaskListView: UIView {
+    
+    lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .insetGrouped)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return tableView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+    
+    private func setupView() {
+        setupHierarchy()
+        setupConstraints()
+        setupConfigurations()
+    }
+    
+    private func setupHierarchy() {
+        addSubview(tableView)
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+    
+    private func setupConfigurations() {
+        backgroundColor = .secondarySystemBackground
+    }
+}
