cc34fb4a33ff9e7b0df39163d432d49175229a70
Diggo Silva <rdiggosilva@gmail.com>
2026-05-14 08:33:19 -0300

    feat: define Tarefa model and TaskPriority enum

---
+++ ListaDeTarefas/App/Model/Tarefa.swift
@@ -0,0 +1,30 @@
+//
+//  Tarefa.swift
+//  ListaDeTarefas
+//
+//  Created by Diggo Silva on 13/05/26.
+//
+
+import UIKit
+
+enum TaskPriority: Int {
+    case low = 0
+    case medium = 1
+    case high = 2
+    
+    var color: UIColor {
+        switch self {
+        case .low: return .systemYellow
+        case .medium: return .systemOrange
+        case .high: return .systemRed
+        }
+    }
+}
+
+struct Tarefa: Identifiable {
+    let id: UUID = UUID()
+    let title: String
+    var isCompleted: Bool = false
+    let priority: TaskPriority
+    let date: Date
+}
---
+++ ListaDeTarefas/App/Scenes/TaskList/TaskListViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  TaskListViewModel.swift
+//  ListaDeTarefas
+//
+//  Created by Diggo Silva on 13/05/26.
+//
+
+import Foundation
