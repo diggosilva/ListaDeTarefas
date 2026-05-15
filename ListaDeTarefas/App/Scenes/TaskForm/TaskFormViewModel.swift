9eb9c6384cdc106c2616973534576629bcd49730
Diggo Silva <rdiggosilva@gmail.com>
2026-05-15 08:29:16 -0300

    feat: implement TaskFormViewModel and task creation

--- ListaDeTarefas/App/Scenes/TaskForm/TaskFormViewModel.swift
+++ ListaDeTarefas/App/Scenes/TaskForm/TaskFormViewModel.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+
+protocol TaskFormViewModelProtocol {
+    func addTask(title: String, priority: TaskPriority.RawValue) -> Tarefa
+}
+
+final class TaskFormViewModel: TaskFormViewModelProtocol {
+    func addTask(title: String, priority: TaskPriority.RawValue) -> Tarefa {
+        let newTarefa = Tarefa(title: title, priority: TaskPriority(rawValue: priority) ?? .low, date: Date())
+        return newTarefa
+    }
+}
