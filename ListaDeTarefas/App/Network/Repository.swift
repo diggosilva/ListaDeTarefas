75eb30c3cb7f9dde2463d2945cedc78bc0d92670
Diggo Silva <rdiggosilva@gmail.com>
2026-05-18 11:56:18 -0300

    feat(network): implement local storage using UserDefaults

--- ListaDeTarefas/App/Network/Repository.swift
+++ ListaDeTarefas/App/Network/Repository.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+protocol RepositoryProtocol {
+    func save(_ tarefas: [Tarefa])
+    func load() -> [Tarefa]
+}
+
+class Repository: RepositoryProtocol {
+    
+    private let taskKey = "task_key"
+    private let userDefaults = UserDefaults.standard
+    
+    func save(_ tarefas: [Tarefa]) {
+        if let data = try? JSONEncoder().encode(tarefas) {
+            userDefaults.set(data, forKey: taskKey)
+        }
+    }
+    
+    func load() -> [Tarefa] {
+        if let data = userDefaults.data(forKey: taskKey) {
+            if let decoded = try? JSONDecoder().decode([Tarefa].self, from: data) {
+                return decoded
+            }
+        }
+        return []
+    }
+}
